0fb79cfcd54617a0316d4d89e5df896e7f9cd381
Ross Keddy <leonzola7@gmail.com>
2018-07-14 14:08:18 -0400

    app listen enable

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//app.listen(3000);
+app.listen(3000);
 console.log("Connected - running on port 3000");
 
 module.exports = app;
