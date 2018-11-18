d80f1a4f007ebda86170e7837ce9da1e9a3616fa
Ross Keddy <leonzola7@gmail.com>
2018-11-18 16:52:37 -0500

    Minor edits

--- config/globalVars.js
+++ config/globalVars.js
@@ -3,6 +3,6 @@
  */
 module.exports = {
     db: 'mongodb://RossKeddy:Artfolio12345@ds137581.mlab.com:37581/artfoliodb',
-    secret: 'There are no stupid questions, just stupid people'
+    secret: 'code'
 };
 /*http://localhost:3000/facebook/callback*/
